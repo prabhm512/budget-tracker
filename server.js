681e2d8021f14fd7bda49f0435492bb8bbae4951
Prabh Singh <prabhm512@gmail.com>
2021-01-04 16:06:55 +1100

    base setup by trilogy, mongo connection established

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// MongoDB Atlas connection
+const PWD = process.env.MONGO_PWD;
+
+mongoose.connect(`mongodb+srv://prabhm512:${PWD}@cluster0.ltepl.mongodb.net/budget`, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});;
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
